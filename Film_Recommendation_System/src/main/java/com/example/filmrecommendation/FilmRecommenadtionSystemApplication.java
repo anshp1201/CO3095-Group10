7c297cb650e9216bdacd515df9f9de109e40ece3
Isah Abubakar <ika3@student.le.ac.uk>
2024-12-22 19:56:28 +0000

    Replaced Film_Recommendation_System context

---
+++ Film_Recommendation_System/src/main/java/com/example/filmrecommendation/FilmRecommenadtionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRecommenadtionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRecommenadtionSystemApplication.class, args);
+	}
+
+}
