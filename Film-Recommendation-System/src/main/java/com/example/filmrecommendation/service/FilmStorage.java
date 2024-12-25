1559a4c71efcb1ec3984a23d8fe1900d4df7b8e7
Walehdulex <dulexw@gmail.com>
2024-12-25 03:38:53 +0000

    Creted Film Model & Film Storage Serice

--- Film-Recommendation-System/src/main/java/com/example/filmrecommendation/service/FilmStorage.java
+++ Film-Recommendation-System/src/main/java/com/example/filmrecommendation/service/FilmStorage.java
@@ -1,5 +1,35 @@
 package com.example.filmrecommendation.service;
 
+import com.example.filmrecommendation.model.Film;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
 public class FilmStorage {
+	private final List<Film> films;
+	
+	
+	public FilmStorage() {
+		films = new ArrayList<>();
+		
+		films.add(new Film("Incetion", "Sci-Fi", "Christopher Nolan", 9.0));
+		films.add(new Film("The Dark Knight", "Action", "Christopher Nolan", 9.0));
+        films.add(new Film("Titanic", "Romance", "James Cameron", 7.9));
+        films.add(new Film("Interstellar", "Sci-Fi", "Christopher Nolan", 8.6));
+	}
+	
+	public List<Film> getAllFilms() {
+		return films;
+	}
+	
+	public List<Film> getRecommendations(String favoriteGenre, List<String> viewedFilms) {
+		return films.stream()
+				.filter(film -> film.getGenre().equals(favoriteGenre) && viewedFilms.contains(film.getTitle()))
+				.sorted((f1, f2) -> Double.compare(f2.getAverageRating(), f1.getAverageRating()))
+				.collect(Collectors.toList());
+
+	}
 
 }
