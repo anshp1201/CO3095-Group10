e62943f528fbb85881b9caeaa01119c57095e9ea
Walehdulex <dulexw@gmail.com>
2024-12-25 05:20:37 +0000

    updated

--- Film-Recommendation-System/src/main/java/com/example/filmrecommendation/model/Film.java
+++ Film-Recommendation-System/src/main/java/com/example/filmrecommendation/model/Film.java
@@ -1,5 +1,68 @@
 package com.example.filmrecommendation.model;
 
-public class Film {
+import java.io.Serializable;
+
+
+
+public class Film implements Serializable{
+	private static final long serialVersionUID =1L;
+	private String title;
+	private String genre;
+	private String director;
+	private double averageRating;
+	
+	
+	public Film() {}
+	
+	public Film(String title, String genre, String director, double averageRating) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.director = director;
+		this.averageRating = averageRating;
+	}
+	
+	
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
+	public String getDirector() {
+		return director;
+	}
+
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+
+	public double getAverageRating() {
+		return averageRating;
+	}
+
+
+	public void setAverageRating(double averageRating) {
+		this.averageRating = averageRating;
+	}
 
 }
+
+
+	
