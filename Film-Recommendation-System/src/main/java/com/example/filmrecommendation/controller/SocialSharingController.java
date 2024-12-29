8f2c9f12d67598aa1c5aff853ebaaf994577fe6c
anshp1201 <anshpetety@gmail.com>
2024-12-29 05:39:56 +0300

    Update SocialSharingController.java

--- Film-Recommendation-System/src/main/java/com/example/filmrecommendation/controller/SocialSharingController.java
+++ Film-Recommendation-System/src/main/java/com/example/filmrecommendation/controller/SocialSharingController.java
@@ -1,1 +1,40 @@
+package com.example.filmrecommendation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.filmrecommendation.model.Film;
+import com.example.filmrecommendation.model.User;
+import com.example.filmrecommendation.service.FilmStorage;
+import com.example.filmrecommendation.service.UserStorage;
+
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
+public class SocialSharingController{
+	
+	/*@Autowired
+	private FilmStorage filmStorage;
+	
+	@Autowired
+	private UserStorage userStorage;*/
+	
+	@GetMapping("/share/{FilmTitle}")
+	public String getShare(HttpSession session, Model model, @PathVariable("FilmTitle") String FilmTitle) {
+		
+		User loggedInUser = (User) session.getAttribute("loggedInUser");
+		String currentFilm = FilmTitle;
+		
+		model.addAttribute("user", loggedInUser);
+		model.addAttribute("film", currentFilm);
+		
+		return "share";
+		
+	}
+	
+}
 
