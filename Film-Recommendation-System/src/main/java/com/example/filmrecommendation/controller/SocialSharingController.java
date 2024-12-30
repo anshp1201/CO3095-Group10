b615087bef2169d207847283291015b585daa440
anshp1201 <anshpetety@gmail.com>
2024-12-30 07:39:43 +0300

    Update SocialSharingController.java

--- Film-Recommendation-System/src/main/java/com/example/filmrecommendation/controller/SocialSharingController.java
+++ Film-Recommendation-System/src/main/java/com/example/filmrecommendation/controller/SocialSharingController.java
@@ -1,10 +1,13 @@
 package com.example.filmrecommendation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.filmrecommendation.model.Film;
 import com.example.filmrecommendation.model.User;
@@ -17,24 +20,29 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class SocialSharingController{
 	
-	/*@Autowired
+	@Autowired
 	private FilmStorage filmStorage;
 	
 	@Autowired
-	private UserStorage userStorage;*/
+	private UserStorage userStorage;
 	
-	@GetMapping("/share/{FilmTitle}")
-	public String getShare(HttpSession session, Model model, @PathVariable("FilmTitle") String FilmTitle) {
-		
-		User loggedInUser = (User) session.getAttribute("loggedInUser");
-		String currentFilm = FilmTitle;
-		
-		model.addAttribute("user", loggedInUser);
-		model.addAttribute("film", currentFilm);
-		
-		return "share";
-		
-	}
+
+	 @RequestMapping("/share")
+	    public String getShare(HttpSession session, Model model, @RequestParam("FilmID") String FilmID) {
+	        User loggedInUser = (User) session.getAttribute("loggedInUser");
+
+	        Film currentFilm = filmStorage.getFilm(FilmID);
+
+	        if (currentFilm == null) {
+	            model.addAttribute("error", "Film not found for FilmID: " + FilmID);
+	            return "error"; 
+	        }
+
+	        model.addAttribute("user", loggedInUser);
+	        model.addAttribute("film", currentFilm);  
+
+	        return "share"; 
+	   }
 	
 }
 
